ef9a6cf4bab9cf3436ea543bc3332be14d4f8b07
Priya Savaliya <pr930067@dal.ca>
2022-03-26 21:37:29 -0300

    key has been changed

--- myapp.js
+++ myapp.js
@@ -81,8 +81,8 @@ var mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : 'database-b00884335-instance-1.cgnewppeezrp.us-east-1.rds.amazonaws.com',
   port     : '3306',
-  user     : user,
-  password :  password,
+  user     : 'admin',
+  password :  'Pr7504.sav',
   
 })
 
